46d079ce4983d8bc9c1111d37664bf52d28634ed
aymarino <aymarino@umich.edu>
2015-03-15 16:54:57 -0400

    Modularized script, added initialization and setter functions

--- snake.js
+++ snake.js
@@ -1,12 +1,32 @@
-var _GRID_LENGTH = 20;
-var _LINK_LENGTH = 20;
-var _LINK_COLOR = "#FF0000";
+var CANVAS_ELEMENT;
+var CANVAS;
 
-var c = document.getElementById("gameboard");
-var ctx = c.getContext("2d");
-c.width = window.innerWidth * 0.7;
-c.height = window.innerHeight * 0.8;
+var X_DIM;
+var Y_DIM;
 
-ctx.fillStyle = _LINK_COLOR;
-// ctx.fillRect(x_pos, y_pos, x_length, y_length)
-ctx.fillRect(2*_GRID_LENGTH,3*_GRID_LENGTH,_LINK_LENGTH,_LINK_LENGTH);
+var GRID_LENGTH = 20;
+var LINK_LENGTH = 20;
+var LINK_COLOR = "#FF0000";
+var GRID_COLOR = "#000000";
+
+function setLink(xPos, yPos) {
+  CANVAS.fillStyle = LINK_COLOR;
+  CANVAS.fillRect(xPos * GRID_LENGTH, yPos * GRID_LENGTH, LINK_LENGTH, LINK_LENGTH);
+}
+
+function removeLink(xPos, yPos) {
+  CANVAS.fillStyle = GRID_COLOR;
+  CANVAS.fillRect(xPos * GRID_LENGTH, yPos * GRID_LENGTH, LINK_LENGTH, LINK_LENGTH);
+}
+
+function initCanvas() {
+  CANVAS_ELEMENT = document.getElementById("gameboard");
+  CANVAS = CANVAS_ELEMENT.getContext("2d");
+
+  CANVAS_ELEMENT.width = LINK_LENGTH * Math.floor(window.innerWidth * 0.7 / LINK_LENGTH);
+  CANVAS_ELEMENT.height = LINK_LENGTH * Math.floor(window.innerHeight * 0.8 / LINK_LENGTH);
+  X_DIM = CANVAS_ELEMENT.width / GRID_LENGTH;
+  Y_DIM = CANVAS_ELEMENT.height / GRID_LENGTH;
+
+  setLink(2, 3);
+}
